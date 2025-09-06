1b70ef7229341b55239d01c0bb02018df7c86dbb
creampuff <soulofabagel@gmail.com>
2025-09-06 10:53:12 -0700

    deleted test files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/HWMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/HWMap.java
@@ -1,18 +1,26 @@
 package org.firstinspires.ftc.teamcode.Projects;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
 public class HWMap {
     public DcMotor fLeftWheel = null;  //control hub port 2
     public DcMotor fRightWheel = null; //control hub port 3
     public DcMotor bLeftWheel = null; //control hub port 1
     public DcMotor bRightWheel = null; //control hub port 0
+    public Servo testServo = null;
+    public WebcamName camera = null;
 
     public void init(HardwareMap hwMap) {
         fLeftWheel = hwMap.dcMotor.get("FL");
         fRightWheel = hwMap.dcMotor.get("FR");
         bLeftWheel = hwMap.dcMotor.get("BL");
         bRightWheel = hwMap.dcMotor.get("BR");
+        testServo = hwMap.servo.get("testServo"); // on  driver hub, input the name within the quotations
+
+        camera = hwMap.get(WebcamName.class, "webcam");
+
         // Get motors from hardware map
         fRightWheel.setDirection(DcMotor.Direction.REVERSE);
         fLeftWheel.setDirection(DcMotor.Direction.FORWARD);
@@ -29,6 +37,7 @@ public class HWMap {
         bRightWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bLeftWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+
         Stop();
     }
 
@@ -37,5 +46,6 @@ public class HWMap {
         fLeftWheel.setPower(0);
         bRightWheel.setPower(0);
         bLeftWheel.setPower(0);
+        testServo.setPosition(1);
     }
 }
