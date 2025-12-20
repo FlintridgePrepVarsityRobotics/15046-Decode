e97b6746e482d45331475ce358ed978c055ffb7d
Eversonhehe <144392936+Eversonhehe@users.noreply.github.com>
2025-12-20 08:48:43 -0800

    mirrored blue tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/redTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/redTele.java
@@ -213,9 +213,9 @@ public class redTele extends LinearOpMode {
             }
 
             // --- Driver control ---
-            double y = -gamepad1.left_stick_y*.8;
-            double x = gamepad1.left_stick_x * -1.1*.8;
-            double rx = gamepad1.right_stick_x*.8;
+            double y = -gamepad1.left_stick_y*.7;
+            double x = gamepad1.left_stick_x * -1.1*.7;
+            double rx = gamepad1.right_stick_x*.7;
 
 
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
@@ -321,11 +321,11 @@ public class redTele extends LinearOpMode {
 
             if (reversingLauncher && flywheelon == false) {
                 robot.launcher.setPower(-0.7);
-                robot.intakeServo.setPower(0);
+                robot.intakeServo.setPower(1);
+                robot.intake.setPower(-.05);
                 if (reverseTimer.milliseconds() >= 500) {
                     reversingLauncher = false;
                     robot.intakeServo.setPower(0);
-
                 }
             }
             telemetry.addData("reverseingLauncher", reversingLauncher);
