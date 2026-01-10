50e06dbacbec36b6cf6292a266c1a88eb90a2d34
LoganWu123 <nothingboo5@gmail.com>
2026-01-09 22:16:01 -0800

    turrettracking code for ilt bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/turretHWMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/turretHWMap.java
@@ -1,7 +1,16 @@
 package org.firstinspires.ftc.teamcode.Projects;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
 
 
 public class turretHWMap {
@@ -9,22 +18,22 @@ public class turretHWMap {
     public DcMotorEx flywheel = null;
     public void init(HardwareMap hwMap) {
         turret = hwMap.get(DcMotorEx.class, "turret");
-        flywheel = hwMap.get(DcMotorEx.class, "launcher");
+//        flywheel = hwMap.get(DcMotorEx.class, "launcher");
 
 
-        turret.setDirection(DcMotor.Direction.FORWARD);
-        flywheel.setDirection(DcMotor.Direction.FORWARD);
+        turret.setDirection(DcMotor.Direction.REVERSE);
+//        flywheel.setDirection(DcMotor.Direction.FORWARD);
 
 
         turret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
         turret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
-        flywheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        flywheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
         Stop();
@@ -33,6 +42,6 @@ public class turretHWMap {
 
     public void Stop() {
         turret.setPower(0);
-        flywheel.setPower(0);
+//        flywheel.setPower(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/turretTrackerOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/turretTrackerOnly.java
@@ -3,34 +3,136 @@ package org.firstinspires.ftc.teamcode.Projects;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
 
 @TeleOp(name = "turretTrackerOnly")
+
 public class turretTrackerOnly extends LinearOpMode {
+    private Limelight3A limelight;
+
     public turretHWMap robot = new turretHWMap();
-    final double TICKS_PER_REV = 294;
-    final double GEAR_RATIO = .3953; // 34/86
+
+    final double TICKS_PER_REV = 294.0;      // GoBilda 5202/5203
+    final double GEAR_RATIO = 0.3953;        // 34 / 86
+    final double MAX_DEGREES = 90.0;
+
+    final double kP = 0.02;
+    final double MIN_POWER_TO_MOVE = 0.05;
+    final double BEARING_TOLERANCE = 1.5;    // degrees
+
+    private VisionPortal visionPortal;
+    private AprilTagProcessor tagProcessor;
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
+
         robot.init(hardwareMap);
 
         robot.turret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.turret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        telemetry.addData("Status", "Initialized, turret pos reset to 0.");
+        tagProcessor = new AprilTagProcessor.Builder().build();
+
+
+
+        telemetry.addLine("Turret Tracker Initialized");
+        telemetry.update();
+
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(0);
+
+        /*
+         * Starts polling for data.  If you neglect to call start(), getLatestResult() will return null.
+         */
+        limelight.start();
+
+        telemetry.addData(">", "Robot Ready.  Press Play.");
         telemetry.update();
 
         waitForStart();
 
-        while (opModeIsActive()){
-            int currentPos = robot.turret.getCurrentPosition();
+        while (opModeIsActive()) {
+            List<FiducialResult> fiducials = result.getFiducialResults();
+            for (FiducialResult fiducial : fiducials) {
+            int id = fiducial.getFiducialId();
+            }
+            LLResult result = limelight.getLatestResult();
+            if (result != null && result.isValid()) {
+                double tx = result.getTx(); // How far left or right the target is (degrees)
+                int encoderTicks = robot.turret.getCurrentPosition();
+                double turretDegrees =
+                        (encoderTicks / (TICKS_PER_REV / GEAR_RATIO)) * 360.0;
 
-            double degrees = (currentPos / (TICKS_PER_REV / GEAR_RATIO)) * 360;
+                double desiredTurretAngle = turretDegrees + tx;
+                desiredTurretAngle = Range.clip(
+                        desiredTurretAngle,
+                        -MAX_DEGREES,
+                        MAX_DEGREES
+                );
 
-            telemetry.addData("Turret Tracking", "");
-            telemetry.addData("Encoder Ticks", currentPos);
-            telemetry.addData("Degrees", "%.2f", degrees);
+                double angleError = desiredTurretAngle - turretDegrees;
+                double motorPower = 0.0;
+
+                if (Math.abs(angleError) > BEARING_TOLERANCE) {
+                    motorPower = angleError * kP;
+
+                    if (Math.abs(motorPower) < MIN_POWER_TO_MOVE) {
+                        motorPower = Math.signum(motorPower) * MIN_POWER_TO_MOVE;
+                    }
+                }
+
+                motorPower = Range.clip(motorPower, -1.0, 1.0);
+                robot.turret.setPower(motorPower);
+
+                telemetry.addData("Turret Angle (deg)", "%.2f", turretDegrees);
+                telemetry.addData("Desired Angle (deg)", "%.2f", desiredTurretAngle);
+                telemetry.addData("Angle Error (deg)", "%.2f", angleError);
+                telemetry.addData("Motor Power", "%.2f", motorPower);
+                telemetry.addData("Encoder Ticks", encoderTicks);
+                telemetry.addData("Target X", tx);
+            } else {
+                telemetry.addData("Limelight", "No Targets");
+            }
+
+            limelight.stop();
+
+
+
             telemetry.update();
         }
+
+        visionPortal.close();
+    }
+
+    public double getAprilTagBearing() {
+
+        if (tagProcessor.getDetections().isEmpty()) {
+            return 0.0;
+        }
+
+        AprilTagDetection tag = tagProcessor.getDetections().get(0);
+
+        if (tag.id != 22) { //blue or red
+            return 0.0;
+        }
+
+        return tag.ftcPose.bearing;
     }
 }
